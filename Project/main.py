70087a2fdf3729cc76adf830aef93b724d8eb4df
SAHOON KIM <sahuni@gmail.com>
2024-11-12 08:40:47 +0900

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -4,4 +4,6 @@ import xlsxProcess as xlp
 filename = "20241016_89900100056880_154004"
 xlp.toExcelErp(filename)
 
-ut.select_folder(True)
+search_item = filename.split('_')[1]
+worker = get_worker(search_item)
+print(worker) #  {"Name" : "홍길동", "Email" : "hong@abcd.co.kr"}
